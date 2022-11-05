e289c5654774414d05a2653ec00e4845296fe63a
castelojb <joaocb14@gmail.com>
2022-11-05 19:20:58 -0300

    chore: use pinv method for ols

--- src/linear_algebra_resolution/ordinary_least_squares.py
+++ src/linear_algebra_resolution/ordinary_least_squares.py
@@ -5,6 +5,6 @@ def least_squares(x: np.ndarray, y: np.ndarray, l2_regulazation=1) -> np.ndarray
 
 	l2_reg_matrix = np.eye(x.shape[1]) * l2_regulazation
 
-	w = np.linalg.inv((x.T @ x) + l2_reg_matrix) @ x.T @ y
+	w = np.linalg.pinv((x.T @ x) + l2_reg_matrix) @ x.T @ y
 
 	return w
