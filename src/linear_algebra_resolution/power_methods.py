1ae99d0eb49c27e07180f3c544d9717763e7c0b8
castelojb <joaocb14@gmail.com>
2022-11-29 17:16:25 -0300

    fix: ajust the logic in power methods

--- src/linear_algebra_resolution/power_methods.py
+++ src/linear_algebra_resolution/power_methods.py
@@ -1,46 +1,55 @@
 import numpy as np
 
+from src.linear_algebra_resolution.gaussian_elimination import gaussian_elimination
+from src.linear_algebra_resolution.lu_decomposition import lu_resolution
+
 
 def regular_power_method(
-    x: np.ndarray,
-    u: np.ndarray = None,
+    a: np.ndarray,
+    x: np.ndarray = None,
+    eps: float = 0.0001,
     iterr_max: int = 100
-) -> tuple[np.ndarray, float]:
+) -> tuple[np.ndarray, float, bool]:
 
-    x = x.copy()
+    a = a.copy()
 
-    u = np.ones(x.shape[0]) if u is None else u.copy()
+    x = np.ones(a.shape[0]) if x is None else x.copy()
+
+    converged = False
 
     eigenvalue = 0
+    last_eigenvalue = eigenvalue
 
     for _ in range(iterr_max):
 
-        u = x @ u
+        x_hat = x / np.linalg.norm(x)
 
-        eigenvalue = u.max()
+        x = a @ x_hat
 
-        u = u / eigenvalue
+        eigenvalue = x_hat @ x
 
-    return u, eigenvalue
+        if abs((eigenvalue - last_eigenvalue) / eigenvalue) < eps:
+            converged = True
+            break
+
+        last_eigenvalue = eigenvalue
+
+    x_hat = x / np.linalg.norm(x)
+
+    return x_hat, eigenvalue, converged
 
 
 def inverse_power_method(
-    x: np.ndarray,
-    u: np.ndarray = None,
+    a: np.ndarray,
+    x: np.ndarray = None,
+    eps: float = 0.0001,
     iterr_max: int = 100
-) -> tuple[np.ndarray, float]:
-    x = x.copy()
+) -> tuple[np.ndarray, float, bool]:
 
-    u = np.ones(x.shape[0]) if u is None else u.copy()
+    a_pinv = np.linalg.pinv(a)
 
-    eigenvalue = 0
+    x_hat, eigenvalue, converged = regular_power_method(a_pinv, x, eps=eps, iterr_max=iterr_max)
 
-    for _ in range(iterr_max):
+    inv_eigenvalue = 1/eigenvalue
 
-        u = x / u
-
-        eigenvalue = u.max()
-
-        u = 1/eigenvalue*u
-
-    return u, eigenvalue
+    return x_hat, inv_eigenvalue, converged
