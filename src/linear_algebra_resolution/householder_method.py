1f3cc822517161832c024a759e5c155692e6dfd3
castelojb <joaocb14@gmail.com>
2022-12-04 20:33:31 -0300

    chore: make a householder method

---
+++ src/linear_algebra_resolution/householder_method.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def __householder_matrix(a: np.ndarray, c: int) -> np.ndarray:
+
+    n = a.shape[0]
+
+    v = np.zeros([n, 1])
+    v1 = np.zeros([n, 1])
+    eye = np.eye(n)
+
+    v[c+1:n, 0] = a[c+1:n, c]
+
+    # for i in range(c+1, n):
+    #     v[i] = a[i, c]
+
+    lv = np.linalg.norm(v, 2)
+
+    # if v[c+1] > 0:
+    #     lv = -lv
+
+    v1[c+1] = lv
+
+    n_vector = v - v1
+    vn = n_vector / np.linalg.norm(n_vector)
+
+    h = eye - 2 * vn @ vn.T
+
+    return h
+
+
+def householder(a: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+
+    n = a.shape[0]
+
+    ab = a.copy()
+    
+    h = np.eye(n)
+    
+    for c in range(n-2):
+        
+        hc = __householder_matrix(ab, c)
+
+        ab = hc @ ab @ hc
+        h = h @ hc
+
+    return ab, h
