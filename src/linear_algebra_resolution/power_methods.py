c116aca4741650689c6cb532a31f0c55aeb543db
castelojb <joaocb14@gmail.com>
2022-12-04 18:02:46 -0300

    chore: fix shifted power method

--- src/linear_algebra_resolution/power_methods.py
+++ src/linear_algebra_resolution/power_methods.py
@@ -53,3 +53,21 @@ def inverse_power_method(
     inv_eigenvalue = 1/eigenvalue
 
     return x_hat, inv_eigenvalue, converged
+
+
+def shifted_power_method(
+    a: np.ndarray,
+    x: np.ndarray = None,
+    mu: float = 0.2,
+    eps: float = 0.0001,
+    iterr_max: int = 100
+) -> tuple[np.ndarray, float, bool]:
+    a = a.copy()
+
+    x = np.ones(a.shape[0]) if x is None else x.copy()
+
+    a_skiped = a - np.eye(a.shape[0]) * mu
+
+    x_hat, eigenvalue, converged = inverse_power_method(a_skiped, x, eps, iterr_max)
+
+    return x_hat, eigenvalue + mu, converged
