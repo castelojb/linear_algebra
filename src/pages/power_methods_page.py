b3e1df35ca35ce1b0bfe28cc8606691a3ab15f3d
castelojb <joaocb14@gmail.com>
2022-12-20 13:47:23 -0300

    fix: slider acepted float numbers

--- src/pages/power_methods_page.py
+++ src/pages/power_methods_page.py
@@ -28,7 +28,7 @@ def show_power_methods_page(a_b: np.ndarray):
 	st.write(inverse_eigvector)
 
 	st.markdown('Potencia com Deslocamento')
-	mu = st.slider('Shifted', min_value=1, max_value=10, step=0.5)
+	mu = st.number_input(label="Shifted", min_value=0.5, step=0.1, max_value=2, value=2, format="%f")
 	shifted_eigvector, shifted_eigvalue, _ = shifted_power_method(a, x=b, mu=mu, iterr_max=1000)
 	st.write(shifted_eigvalue)
 	st.write(shifted_eigvector)
